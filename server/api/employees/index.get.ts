0e887ff318b203e9736f9b4dbec7ac192a4961f8
Hilmi Razib Yusuf <62739416+hilmirazib@users.noreply.github.com>
2025-09-14 10:26:00 +0700

    refactor: use response wrapper in API handlers

--- server/api/employees/index.get.ts
+++ server/api/employees/index.get.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 import { EmployeeTransformer } from '../../transformers/employee.transformer'
 import { defineEventHandler } from 'h3'
+import { ok } from '../../../server/utils/responseWrapper'
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async () => {
   const employees = await prisma.employee.findMany({ where: { isDeleted: false } })
-  return { success: true, data: EmployeeTransformer.collection(employees) }
+  return ok(EmployeeTransformer.collection(employees))
 })
